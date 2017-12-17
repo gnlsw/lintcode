792db739dea057e9d3f556a32627a81efec73762
gnlsw <today_wsl@163.com>
2017-12-17 14:06:07 +0800

    逐个bit进行加法

---
+++ problem_1/problem_1 - 副本.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+class Solution {
+public:
+    /*
+     * @param : An integer
+     * @param : An integer
+     * @return: The sum of a and b
+     */
+    int aplusb(int a, int b)
+	{
+        // write your code here
+        int sum = 0;
+        int count = 0;
+
+		int bit_carry = 0;
+        /* ��bit�λ���д�� */
+        do{
+			int bit_a = a & (1 << count);
+			int bit_b = b & (1 << count);
+			a = a & (~(1 << count));
+			b = b & (~(1 << count));
+            int bit_sum = bit_a ^ bit_b ^ bit_carry;   // ��Ӧλ֮�
+			sum |= bit_sum;
+
+			if (0 == ((bit_a & bit_b) | (bit_a & bit_carry) | (bit_b & bit_carry)))
+			{
+				bit_carry = 0;
+			}
+			else
+			{
+				bit_carry = 1 << (count + 1);
+			}
+			count++;
+        }while(a != 0 || b != 0 || bit_carry != 0);
+
+		return sum;
+    }
+};
+
+int main()
+{
+    Solution s;
+    s.aplusb(4 ,4);
+
+    return 0;
+}
